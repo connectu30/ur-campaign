34f9397bbc6f9653cdd22f550c09c46a05b82eec
sam0 <sam0@6gram.co>
2018-12-10 19:07:31 +0900

    remove_apikey

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -5,14 +5,6 @@ import VueFire from 'vuefire'
 import 'firebase/firestore'
 import 'firebase/storage'
 
-var config = {
-    apiKey: "AIzaSyCg2UgyBB1qKuRzbEDZQu9fbWmn3KgKjA0",
-    authDomain: "ur-campaign.firebaseapp.com",
-    databaseURL: "https://ur-campaign.firebaseio.com",
-    projectId: "ur-campaign",
-    storageBucket: "ur-campaign.appspot.com",
-    messagingSenderId: "770312569261"
-}
 var settings = { timestampsInSnapshots: true }
 
 firebase.initializeApp(config)
